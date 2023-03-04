9a3e2e4b14773d88c4096ff45c910c6a0de26d49
Andreea <andreea.stoenescu@hotmail.com>
2023-03-04 10:42:37 +0000

    use if else

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ fn add(num_one: i32, num_two: i32) -> i32 {
     return num_one + num_two;
 }
 fn main() {
-    let foo = add(10, 5);
-    println!("{0} {0}", foo);
+    let total = add(10, 5);
+
+    if total > 50 {
+        println!("You qualify for free shipping!");
+    } else if  total > 20 {
+        println!("If you add more items, you can qualify for free shipping.");
+    } else {
+        println!("No free shipping.");
+    }
+    println!("{:?}", total);
 }
